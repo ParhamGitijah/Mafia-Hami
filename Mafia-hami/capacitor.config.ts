c4288145cb3ac1296365817424c194fb1ac3a896
Parham <parham62@yahoo.com>
2022-10-14 20:09:09 +0200

    Add Capacitor to create native application

---
+++ Mafia-hami/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pagi.appa',
+  appName: 'mafia-hami',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
